e54eca186cf5da9ca8f72c5655e1c18ce2dde2d6
Nico <nicomilk@gmail.com>
2020-07-28 16:16:41 +0200

    before merge

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        
+        $users = User::all();
+        // $users = DB::table('users')->get();
+        
+        $response = (object) ["users" => $users];
+        
+        return response()->json($response) ;
+    }
+
 }
